9583bcf4ce7ebe6dc8fdeacd15a24d11c47943d3
vitaminochres3l <vitaminochres3l@icloud.com>
2025-03-18 19:31:53 +0200

    📝 Commit 0 with SOLIDITY file

--- Contract_0.sol
+++ Contract_0.sol
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleStorage_0 {
+contract HelloWorld_0 {
     uint256 public randomNumber;
     string public message;
 
     constructor() {
-        randomNumber = 43;
-        message = "Hello from commit 0 - 2025-03-18 19:31:43";
+        randomNumber = 21;
+        message = "Hello from commit 0 - 2025-03-18 19:31:53";
     }
 
     function getMessage() public view returns (string) {
